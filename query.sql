ea722c6dfc31ee0b146212f1341bd5cdfd205d10
ramprasathmk <mkramprasath12@gmail.com>
2024-08-13 23:29:59 +0545

    feat: Uploaded SQL-Query file

---
+++ query.sql
@@ -0,0 +1,20 @@
+-- DATABASE Creation
+CREATE DATABASE ridesharing;
+
+-- Using `ridesharing` Database
+use ridesharing;
+
+-- `users` TABLE Creation
+CREATE TABLE users (
+    Id INT(3) UNSIGNED AUTO_INCREMENT,
+    Name VARCHAR(20) NOT NULL,
+    Phone Bigint(10) NOT NULL UNIQUE,
+    -- Age int(3) NOT NULL,
+    DOB DATE NOT NULL,
+    Address VARCHAR(30) NOT NULL,
+    City VARCHAR(15) NOT NULL,
+    Email VARCHAR(20) NOT NULL UNIQUE,
+    Username VARCHAR(15) NOT NULL UNIQUE,
+    Password VARCHAR(15) NOT NULL UNIQUE,
+    PRIMARY KEY(Id)
+);
